1ec4b2c1c593af4bec89cbb2ee04c209282b3bf6
eutterba <eutterba@DESKTOP-5VCIKN6>
2024-09-05 19:52:45 -0400

    add java and FindDuplicates to the repository

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # my lab3
 
-public static int[] duplist(int[] a){
-	size=a.lenght;
-    int count=0;
-    int current=1;
-    int numofdups=0;
-    int[] duplist;
-    while (count<size-1){
-        while (current<size){
-            if(a[count]==a[current]){
-                duplist[numofdups]=a[count];
-                numofdups++;
-                break;
-            }
-            current++;
-        }
-        count++;
-        current=count-1;
-    }
-    return duplist;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-}
+public static List<Integer> findModeNestedLoops(List<Integer> l){
+		int size=l.size();
+		int count=0;
+		int current=1;
+		List<Integer> duplist = new ArrayList<>();
+		while(count<size-1) {
+			while(current<size) {
+				if(l.get(count)==l.get(current) && (duplist.contains(l.get(count))==false)) {
+					duplist.add(l.get(count));
+					break;
+				}
+				current++;
+			}
+			count++;
+			current=count+1;
+		}
+		
+		return duplist;
+
+	}
+
---
+++ java/FindDuplicates.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDuplicates {
+
+	public static List<Integer> findModeNestedLoops(List<Integer> l) {
+		int size = l.size();
+		int count = 0;
+		int current = 1;
+		List<Integer> duplist = new ArrayList<>();
+		while (count < size - 1) {
+			while (current < size) {
+				if (l.get(count) == l.get(current) && (duplist.contains(l.get(count)) == false)) {
+					duplist.add(l.get(count));
+					break;
+				}
+				current++;
+			}
+			count++;
+			current = count + 1;
+		}
+
+		return duplist;
+
+	}
+
+	public static void main(String[] args) {
+		// some test strings:
+		List<Integer> sample1 = new ArrayList<Integer>(Arrays.asList(3, 7, 5, 6, 7, 4, 8, 5, 7, 66));
+		List<Integer> sample2 = new ArrayList<Integer>(Arrays.asList(3, 5, 6, 4, 4, 5, 66, 6, 7, 6));
+		List<Integer> sample3 = new ArrayList<Integer>(Arrays.asList(3, 0, 5, 1, 0));
+		List<Integer> sample4 = new ArrayList<Integer>(Arrays.asList(3));
+		System.out.println("Sample 1: " + findModeNestedLoops(sample1));
+		System.out.println("Sample 2: " + findModeNestedLoops(sample2));
+		System.out.println("Sample 3: " + findModeNestedLoops(sample3));
+		System.out.println("Sample 4: " + findModeNestedLoops(sample4));
+	}
+}
